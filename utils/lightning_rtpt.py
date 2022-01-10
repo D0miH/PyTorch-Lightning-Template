49c371b3acbbbe95ef6f5c8b19b7c1d72de1de0c
Dominik <d0mih.github@gmail.com>
2022-01-10 17:55:38 +0100

    autoformatting

--- utils/lightning_rtpt.py
+++ utils/lightning_rtpt.py
@@ -6,14 +6,14 @@ from rtpt import RTPT
 
 class LightningRtpt(pl.Callback):
     def __init__(
-            self,
-            name_initials: str,
-            experiment_name: str,
-            max_iterations: int,
-            iteration_start=0,
-            moving_avg_window_size=20,
-            update_interval=1,
-            precision=2
+        self,
+        name_initials: str,
+        experiment_name: str,
+        max_iterations: int,
+        iteration_start=0,
+        moving_avg_window_size=20,
+        update_interval=1,
+        precision=2
     ):
         super().__init__()
 
@@ -32,4 +32,3 @@ class LightningRtpt(pl.Callback):
 
     def on_train_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule, unused: Optional = None) -> None:
         self.rtpt.step()
-
